9a831707e3cf188377d980668a857872d911e1a0
sebalo3888 <87337147+sebalo3888@users.noreply.github.com>
2021-07-15 13:22:37 -0300

    Add files via upload

---
+++ js/recetario.js
@@ -0,0 +1,3 @@
+var ingrediente1 = prompt('Ingresa 1 ingrediente para una receta')
+var ingrediente2 = prompt('Ingresa otro ingrediente para una receta')
+var ingrediente3 = prompt('Ingresa un tercer ingrediente para una receta')
